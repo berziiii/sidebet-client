40523df72e7c5e3913d4b6494355f9d09aacc988
berziiii <brian.berzellini@thelevelup.com>
2018-09-18 12:15:16 -0400

    deploy

--- src/core/AppConfig.ts
+++ src/core/AppConfig.ts
@@ -1,8 +1,7 @@
 export const AppConfig = {
     Settings: {
         Server: {
-            // baseUrl: "https://sidebet-api.herokuapp.com",
-            baseUrl: `${process.env.NODE_ENV ? "http://localhost:3000" : "https://sidebet-api.herokuapp.com"}`,
+            baseUrl: `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sidebet-api.herokuapp.com"}`,
             timeout: 30000,
             context: "SideBet",
         }
